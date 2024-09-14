014f0560b5976ac94f4c02b32c42f7479f2d8927
RicardoAntonGustavOtto <contact@thedutchdev.com>
2024-09-14 23:11:00 +0200

    .

--- server.js
+++ server.js
@@ -24,6 +24,22 @@ const CHIPPS = [
 app.use(bodyParser.json());
 
 app.post("/proxy/chat", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "https://howtoai.tech");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   const { number, messageList } = req.body;
 
   const applicationId = CHIPPS[number].applicationId;
